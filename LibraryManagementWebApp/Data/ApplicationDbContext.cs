b20c1c2ef6593f22c02f50779003b3c443756883
Sanish_Bal <sbalnet369@gmail.com>
2024-05-01 14:33:42 +0545

    First commit MVC library management mini project

---
+++ LibraryManagementWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using LibraryManagementWebApp.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementWebApp.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        
+    }
+}
---
+++ LibraryManagementWebApp/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryManagementWebApp.Web.Models.Entities
+{
+    public class Student
+    {
+ 
+        public int StudentId { get; set; }
+
+      
+        public string StudentName { get; set; }
+
+      
+        public string StudentEmail { get; set; }
+    }
+
+}
+
